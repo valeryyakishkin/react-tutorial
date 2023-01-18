35ab4c22720b94c8935b242d9c88dc6ca9d14510
valeryyakishkin <velyakishkin@gmail.com>
2023-01-18 21:31:02 +0300

    lesson-2: Components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Lesson from "./02_Components/Lesson";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+React.createElement("div", null, "Simple div with text");
+React.createElement("input", { className: "just-class" });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Lesson />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
