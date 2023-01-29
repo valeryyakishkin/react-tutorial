d353aba2c4c1b15bf19070f991dc5bd7fbd1558b
valeryyakishkin <velyakishkin@gmail.com>
2023-01-29 20:14:36 +0300

    lesson-4: Props (part 1)

--- src/03_state/Lesson.jsx
+++ src/03_state/Lesson.jsx
@@ -17,26 +17,26 @@ import React, { Component } from "react";
 // };
 
 class CounterButton extends Component {
-    state  = {
-        counter: 0,
-    }
+  state = {
+    counter: 0,
+  };
 
-    handleClick = () => {
-        this.setState(({ counter }) => ({
-            counter: ++counter,
-        }))
-    }
+  handleClick = () => {
+    this.setState(({ counter }) => ({
+      counter: ++counter,
+    }));
+  };
 
-    render() {
-        const { counter } = this.state;
+  render() {
+    const { counter } = this.state;
 
-        return (
-            <div>
-                <div>{counter}</div>
-                <button onClick={this.handleClick}>+1</button>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <div>{counter}</div>
+        <button onClick={this.handleClick}>+1</button>
+      </div>
+    );
+  }
 }
 
 export default CounterButton;
