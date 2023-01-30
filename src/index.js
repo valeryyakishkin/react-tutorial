77fa5e54a1428a27fca78b97379b799b4f884f36
valeryyakishkin <velyakishkin@gmail.com>
2023-01-30 19:36:13 +0300

    lesson-5: Props (part 2)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Lesson from "./04-props/Lesson";
+import { Lesson, Counter, Button } from "./05_props/Lesson";
 
 React.createElement("div", null, "Simple div with text");
 React.createElement("input", { className: "just-class" });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Lesson />);
+root.render(<Lesson child={<Button />}><Counter /></Lesson>);
