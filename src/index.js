64611696b681a2f576b29509faf18d302635c929
valeryyakishkin <velyakishkin@gmail.com>
2023-01-31 17:35:44 +0300

    lesson-7: Conditional Rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./06_events/Lesson";
+import App from "./07_conditional-rendering/Lesson";
 
 React.createElement("div", null, "Simple div with text");
 React.createElement("input", { className: "just-class" });
