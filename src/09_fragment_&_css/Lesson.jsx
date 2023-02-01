72e197d03d8e09e24cc48cc1a5dbd893be2432c3
valeryyakishkin <velyakishkin@gmail.com>
2023-02-01 19:15:49 +0300

    lesson-9: Fragment & CSS

---
+++ src/09_fragment_&_css/Lesson.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import './styles.css';
+
+// const Columns = () => ([
+//     <td key="1">Hello</td>,
+//     <td key="2">world</td>,
+// ])
+
+const styles = {color: 'red', textTransform: 'uppercase'};
+
+const Columns = () => (
+    <Fragment>
+        <td style={styles}>Inline</td>
+        <td className="title">Simple class</td>
+    </Fragment>
+);
+
+const Table = () => (
+    <table>
+        <tbody>
+            <tr>
+                <Columns />
+            </tr>
+        </tbody>
+    </table>
+)
+
+export default Table;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./08_lists_and_keys/Lesson";
+import Table from "./09_fragment_&_css/Lesson";
 
 React.createElement("div", null, "Simple div with text");
 React.createElement("input", { className: "just-class" });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Table />);
