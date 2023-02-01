34987ae64eeb4e85b68034277551489223f7606c
valeryyakishkin <velyakishkin@gmail.com>
2023-02-01 19:16:28 +0300

    minor fixes

--- src/09_fragment_&_css/Lesson.jsx
+++ src/09_fragment_&_css/Lesson.jsx
@@ -1,28 +1,28 @@
 import { Fragment } from "react";
-import './styles.css';
+import "./styles.css";
 
 // const Columns = () => ([
 //     <td key="1">Hello</td>,
 //     <td key="2">world</td>,
 // ])
 
-const styles = {color: 'red', textTransform: 'uppercase'};
+const styles = { color: "red", textTransform: "uppercase" };
 
 const Columns = () => (
-    <Fragment>
-        <td style={styles}>Inline</td>
-        <td className="title">Simple class</td>
-    </Fragment>
+  <Fragment>
+    <td style={styles}>Inline</td>
+    <td className="title">Simple class</td>
+  </Fragment>
 );
 
 const Table = () => (
-    <table>
-        <tbody>
-            <tr>
-                <Columns />
-            </tr>
-        </tbody>
-    </table>
-)
+  <table>
+    <tbody>
+      <tr>
+        <Columns />
+      </tr>
+    </tbody>
+  </table>
+);
 
 export default Table;
