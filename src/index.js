050c1ab172f5655f50f8eec37790f90e1ebce0e6
valeryyakishkin <velyakishkin@gmail.com>
2023-02-04 17:40:47 +0300

    lesson-14: React Portal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Lesson from "./13_high_order_components/Lesson";
+import Lesson from "./14_react_portal/Lesson";
 
 React.createElement("div", null, "Simple div with text");
 React.createElement("input", { className: "just-class" });
