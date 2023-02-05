b756bb2a5f4b800e44ecb36f2dc19c05ce42b804
valeryyakishkin <velyakishkin@gmail.com>
2023-02-05 18:59:22 +0300

    lesson-16: React - Work with API (Part 1)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Lesson from "./15_react_context/Lesson";
+import News from "./16_work_with_API/News";
 
 React.createElement("div", null, "Simple div with text");
 React.createElement("input", { className: "just-class" });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Lesson />);
+root.render(<News />);
